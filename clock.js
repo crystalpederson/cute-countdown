110339c04e36a91cb911f65040fb814ae93895d3
Crystal Pederson <crystalpederson88@gmail.com>
2021-10-08 17:00:25 -0700

    added selections to options, cleaned up countdown algos

--- clock.js
+++ clock.js
@@ -1,9 +1,3 @@
-// setInterval(
-//     function() {
-//         let date = new Date();
-//         document.getElementById("clock").innerHTML = date.getDay() + ":" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-//  }, 1000);
-
 
 //pick year dynamically
 const today = new Date();
@@ -17,31 +11,32 @@ const holidays = {
     christmas: 'December 25, 2021 00:00:00'
 }
 
+  //set value of selected holiday
+  let selectedHoliday = holidays['thanksgiving']
+  
+  //set a parameter for date you want to countdown towards
+  let countDownDate = new Date(selectedHoliday);
 
-function halloween() {
-
- //set a parameter for date you want to countdown towards
-  let countDownDate = new Date(holidays['halloween']);
-
-  // Find the distance between now and the count down date
+  // Find the difference in milliseconds between now and the count down date
   let distance = countDownDate.getTime() - today.getTime();
 
-  // Time calculations for days, hours, minutes and seconds
+  // Convert milliseconds to days
   let days = Math.floor(distance / (1000 * 60 * 60 * 24)) + 1;
-  //let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  
-  //check to see if halloween has passed yet
-    //if so, count down to halloween of next year
+ 
+  //check to see if holiday has passed yet
+    //if so, count down to holiday of next year
   if(days < 0){
     days += 365
   }
-  
+
+  function halloween(dayValue) {
+
   //check to see if today is halloween. If so, say that it's halloween.
-  if(days === 0){
+  if(dayValue === 0){
     document.getElementById("countdown").innerHTML = "Today is Halloween!"
   }else{
     // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + " days until " + "Halloween!";
+  document.getElementById("countdown").innerHTML = dayValue + " days until " + "Halloween!";
   }
   
   let page = document.querySelector('body')
@@ -52,25 +47,9 @@ function halloween() {
   gif.src = "images/pusheen_witch.gif";
 }
 
-//load the page
-//backslached all the function calls to see if we can invoke
-//the func when we click the button on popup.html
-//halloween();
-
-
 //created the same func for thanksgiving and added a new gif
-function thanksgiving() {
+function thanksgiving(days) {
 
-  //set a parameter for date you want to countdown towards
-   let countDownDate = new Date(holidays['thanksgiving']);
- 
-   // Find the distance between now and the count down date
-   let distance = countDownDate.getTime() - today.getTime();
- 
-   // Time calculations for days, hours, minutes and seconds
-   let days = Math.floor(distance / (1000 * 60 * 60 * 24)) + 1;
-   //let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   
    //check to see if Thanksgiving has passed yet
      //if so, count down to Thanksgiving of next year
    if(days < 0){
@@ -86,36 +65,16 @@ function thanksgiving() {
    }
    
    let page = document.querySelector('body')
-   page.setAttribute("style", "background-color: orange;")
+   page.setAttribute("style", "background-color: #faefa5;")
    
    //add gif
    let gif = document.getElementById("displayImg")
    gif.src = "images/pusheen_thanksgiving.gif";
  }
  
- //load the page
- //thanksgiving();
-
-
  //created the func for christmas and added a new gif
- function christmas() {
+ function christmas(days) {
 
-  //set a parameter for date you want to countdown towards
-   let countDownDate = new Date(holidays['christmas']);
- 
-   // Find the distance between now and the count down date
-   let distance = countDownDate.getTime() - today.getTime();
- 
-   // Time calculations for days, hours, minutes and seconds
-   let days = Math.floor(distance / (1000 * 60 * 60 * 24)) + 1;
-   //let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   
-   //check to see if Christmas has passed yet
-     //if so, count down to christmas of next year
-   if(days < 0){
-     days += 365
-   }
-   
    //check to see if today is christmas. If so, say that it's halloween.
    if(days === 0){
      document.getElementById("countdown").innerHTML = "Today is Christmas!"
@@ -125,7 +84,7 @@ function thanksgiving() {
    }
    
    let page = document.querySelector('body')
-   page.setAttribute("style", "background-color: orange;")
+   page.setAttribute("style", "background-color: #bdf4ff;")
    
    //add gif
    let gif = document.getElementById("displayImg")
@@ -133,4 +92,4 @@ function thanksgiving() {
  }
  
  //load the page
- //christmas();
+thanksgiving(days);
--- options.js
+++ options.js
@@ -1,48 +1,78 @@
-let page = document.getElementById("buttonDiv");
-let selectedClassName = "current";
-const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d"];
-//const presetHolidays = [["#3aa757", "halloween"], ["#e8453c", "thanksgiving"], ["#f9bb2d", "christmas"];
 
+//allows users to change which holiday is selected for the countdown
+function save_Holiday(){
 
-// Reacts to a button click by marking the selected button and saving the selection
-function handleButtonClick(event) {
-  // Remove styling from the previously selected color
-  let current = event.target.parentElement.querySelector(
-    `.${selectedClassName}`
-  );
-  if (current && current !== event.target) {
-    current.classList.remove(selectedClassName);
-  }
-
-  // Mark the button as selected
-  let color = event.target.dataset.color;
-  event.target.classList.add(selectedClassName);
-  chrome.storage.sync.set({ color });
+  let chosenHoliday = document.getElementById('holidaySelection').value;
+  
+  chrome.storage.sync.set({
+    currentHoliday: chosenHoliday
+  },
+  
+  //update status to let user know options were saved.
+  function(){
+    let status = document.getElementById('status');
+    status.textContent= 'Countdown holiday changed to ' + chosenHoliday + "!";
+  }); 
 }
 
-// Add a button to the page for each supplied color
-function constructOptions(buttonColors) {
-  chrome.storage.sync.get("color", (data) => {
-    let currentColor = data.color;
-
-    // For each color we were provided…
-    for (let buttonColor of buttonColors) {
-      // …create a button with that color…
-      let button = document.createElement("button");
-      button.dataset.color = buttonColor;
-      button.style.backgroundColor = buttonColor;
-
-      // …mark the currently selected color…
-      if (buttonColor === currentColor) {
-        button.classList.add(selectedClassName);
-      }
-
-      // …and register a listener for when that button is clicked
-      button.addEventListener("click", handleButtonClick);
-      page.appendChild(button);
-    }
-  });
+//restores select box and checkbox stat using the preferences stored in chrome.storage.
+function restore_options(){
+  chrome.storage.sync.get({
+    currentHoliday: 'Halloween'
+  }, function(items){
+    document.getElementById('holidaySelection').value = items.currentHoliday;
+  })
 }
 
-// Initialize the page by constructing the color options
-constructOptions(presetButtonColors);
+document.addEventListener('DOMContentLoaded', restore_options)
+document.getElementById('save').addEventListener('click', save_Holiday);
+
+
+// let page = document.getElementById("buttonDiv");
+// let selectedClassName = "current";
+// const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d"];
+// //const presetHolidays = [["#3aa757", "halloween"], ["#e8453c", "thanksgiving"], ["#f9bb2d", "christmas"];
+
+
+// // Reacts to a button click by marking the selected button and saving the selection
+// function handleButtonClick(event) {
+//   // Remove styling from the previously selected color
+//   let current = event.target.parentElement.querySelector(
+//     `.${selectedClassName}`
+//   );
+//   if (current && current !== event.target) {
+//     current.classList.remove(selectedClassName);
+//   }
+
+//   // Mark the button as selected
+//   let color = event.target.dataset.color;
+//   event.target.classList.add(selectedClassName);
+//   chrome.storage.sync.set({ color });
+// }
+
+// // Add a button to the page for each supplied color
+// function constructOptions(buttonColors) {
+//   chrome.storage.sync.get("color", (data) => {
+//     let currentColor = data.color;
+
+//     // For each color we were provided…
+//     for (let buttonColor of buttonColors) {
+//       // …create a button with that color…
+//       let button = document.createElement("button");
+//       button.dataset.color = buttonColor;
+//       button.style.backgroundColor = buttonColor;
+
+//       // …mark the currently selected color…
+//       if (buttonColor === currentColor) {
+//         button.classList.add(selectedClassName);
+//       }
+
+//       // …and register a listener for when that button is clicked
+//       button.addEventListener("click", handleButtonClick);
+//       page.appendChild(button);
+//     }
+//   });
+// }
+
+// // Initialize the page by constructing the color options
+// constructOptions(presetButtonColors);
